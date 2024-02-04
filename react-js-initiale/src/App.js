4837e2f8b5b5115cb3d4c5bd6c591fe737cb1135
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-05 00:55:18 +0100

    first edit

---
+++ react-js-initiale/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Banner from "./component/Banner";
+
+function App() {
+  return (
+    <>
+   <Banner/>
+    </>
+  );
+}
+
+export default App;
