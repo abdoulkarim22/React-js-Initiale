5de299e99eddf5af464b5907f06d724f8a1c3bb8
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-05 13:32:41 +0100

    function no copy

--- react-js-initiale/src/components/Mycars.jsx
+++ react-js-initiale/src/components/Mycars.jsx
@@ -2,14 +2,22 @@ import React, { Component } from "react";
 import Car from "./Cars";
 
 class Mycars extends Component{
-    render (){
 
+
+noCopy = () => {
+  alert('merci de ne pas copier le texte')
+}
+
+    render (){
         return (  
             <>
-              <h1>{this.props.title}</h1>
+             <div>
+             <h1>{this.props.title}</h1>
+              <p onCopy={this.noCopy}>Lorem ipsum dolor sit amet consectetur adipisicing elit.sed, mus.</p>
               <Car color='red'>Ford</Car>
               <Car olor='green'>Mercedes</Car>
               <Car color="black"></Car>
+             </div>
             </>
         )
     }
