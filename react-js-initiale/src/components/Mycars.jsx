d3873697e50df4934a19fc70fbd7cf2ff0aad51b
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-07 11:28:40 +0100

    add table

--- react-js-initiale/src/components/Mycars.jsx
+++ react-js-initiale/src/components/Mycars.jsx
@@ -45,15 +45,15 @@ addYear = () => {
 
         return ( 
             <>
-             <div>
-
-             <h1>{this.props.title}</h1> 
-             <button onClick={this.addYear}>+10 ans</button>
-              <Car color={this.state.voitures[0].color} year={ newyear - this.state.voitures[0].year + 'ans'}>{this.state.voitures[0].name}</Car>
-              <Car color={this.state.voitures[1].color} year={ newyear - this.state.voitures[1].year + 'ans'}>{this.state.voitures[1].name}</Car>
-              <Car color={this.state.voitures[2].color} year={ newyear - this.state.voitures[2].year + 'ans'}>{this.state.voitures[2].name}</Car>
-        
-             </div>
+       
+            <table>
+               <h1>{this.props.title}</h1> 
+               <button onClick={this.addYear}>+10 ans</button>
+               <Car color={this.state.voitures[0].color} year={ newyear - this.state.voitures[0].year + 'ans'}>{this.state.voitures[0].name}</Car>
+               <Car color={this.state.voitures[1].color} year={ newyear - this.state.voitures[1].year + 'ans'}>{this.state.voitures[1].name}</Car>
+               <Car color={this.state.voitures[2].color} year={ newyear - this.state.voitures[2].year + 'ans'}>{this.state.voitures[2].name}</Car>
+            </table>
+            
             </>
         )
     }
