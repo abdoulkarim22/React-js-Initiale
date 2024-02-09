a85d4ae628151d5b4fed7582b4d98d0a28eec822
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-09 11:29:38 +0100

    commentaires file car and Mycars

--- react-js-initiale/src/components/Cars.jsx
+++ react-js-initiale/src/components/Cars.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import {  } from "../styles/StylesCar.css";
+// import React from "react";
+// import {  } from "../styles/StylesCar.css";
 
-const Car = ({children,color,year}) => {
+// const Car = ({children,color,year}) => {
 
-// const colorInfo = color ? (<p>Couleur: {color}</p>) :(<p>Couleur: Néant</p>);
+// // const colorInfo = color ? (<p>Couleur: {color}</p>) :(<p>Couleur: Néant</p>);
 
-if (children) {
-    return(
-        <>
-            <tr>
-              <td><p>Marque: {children}</p></td>
-              <td><p>Année: {year}</p></td>
-              <td><p>Color: {color}</p></td>
-            </tr>
-        </>
-       )
+// if (children) {
+//     return(
+//         <>
+//             <tr>
+//               <td><p>Marque: {children}</p></td>
+//               <td><p>Année: {year}</p></td>
+//               <td><p>Color: {color}</p></td>
+//             </tr>
+//         </>
+//        )
     
-}else{
- return null;
+// }else{
+//  return null;
 
-}
+// }
  
-}
+// }
 
-export default Car
+// export default Car
--- react-js-initiale/src/components/Mycars.jsx
+++ react-js-initiale/src/components/Mycars.jsx
@@ -1,66 +1,66 @@
-import React, { Component,Fragment } from "react";
-import Car from "./Cars";
+// import React, { Component,Fragment } from "react";
+// import Car from "./Cars";
 
-class Mycars extends Component{
+// class Mycars extends Component{
    
 
-// noCopy = () => {
-//   alert('merci de ne pas copier le texte')
-// }
+// // noCopy = () => {
+// //   alert('merci de ne pas copier le texte')
+// // }
 
-state = {
-    voitures : [
-        {
-          name: "Ford",
-          year: "2000",
-          color:"red",
-        },
-        {
-          name: "Mercedes",
-          year: "1876", 
-          color:"green",
-        },
-        {
-          name: "Yaris",
-          year: "1879",
-          color:"Black",
-        }
-    ]
-}
+// // state = {
+// //     voitures : [
+// //         {
+// //           name: "Ford",
+// //           year: "2000",
+// //           color:"red",
+// //         },
+// //         {
+// //           name: "Mercedes",
+// //           year: "1876", 
+// //           color:"green",
+// //         },
+// //         {
+// //           name: "Yaris",
+// //           year: "1879",
+// //           color:"Black",
+// //         }
+// //     ]
+// // }
 
-addYear = () => { 
-    const updatedstate = this.state.voitures.map((param) => {
-        return param.year -= 10;
-    })
+// // addYear = () => { 
+// //     const updatedstate = this.state.voitures.map((param) => {
+// //         return param.year -= 10;
+// //     })
 
 
-    this.setState ({
-        updatedstate
-    })
-}
+// //     this.setState ({
+// //         updatedstate
+// //     })
+// // }
 
-    render (){ 
+//     render (){ 
 
-        const newyear = new Date().getFullYear();  
+//         // const newyear = new Date().getFullYear();  
 
-        return ( 
-            <>
-            <h1>{this.props.title}</h1> 
-            <div className="divButton"><button onClick={this.addYear}>+10 ans</button></div>
-            <table className="carsTable">
-                <tr>
-                    <th>Marque</th>
-                    <th>Age</th>
-                    <th>Color</th>
-                </tr>
-               <Car color={this.state.voitures[0].color} year={ newyear - this.state.voitures[0].year + 'ans'}>{this.state.voitures[0].name}</Car>
-               <Car color={this.state.voitures[1].color} year={ newyear - this.state.voitures[1].year + 'ans'}>{this.state.voitures[1].name}</Car>
-               <Car color={this.state.voitures[2].color} year={ newyear - this.state.voitures[2].year + 'ans'}>{this.state.voitures[2].name}</Car>
+//         return ( 
+//             <>
+//             {/* <h1>{this.props.title}</h1> 
+//             <div className="divButton"><button onClick={this.addYear}>+10 ans</button></div>
+//             <table className="carsTable">
+//                 <tr>
+//                     <th>Marque</th>
+//                     <th>Age</th>
+//                     <th>Color</th>
+//                 </tr>
+//                <Car color={this.state.voitures[0].color} year={ newyear - this.state.voitures[0].year + 'ans'}>{this.state.voitures[0].name}</Car>
+//                <Car color={this.state.voitures[1].color} year={ newyear - this.state.voitures[1].year + 'ans'}>{this.state.voitures[1].name}</Car>
+//                <Car color={this.state.voitures[2].color} year={ newyear - this.state.voitures[2].year + 'ans'}>{this.state.voitures[2].name}</Car>
 
-            </table>
+//             </table> */}
             
-            </>
-        )
-    }
-}
-export default Mycars;
+//             </>
+//         )
+//     }
+// }
+// export default Mycars;
