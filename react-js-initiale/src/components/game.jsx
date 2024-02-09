36b500df95ddbc88a17d53f37f30432a365f6234
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-09 11:39:58 +0100

    new file Game.jsx

---
+++ react-js-initiale/src/components/game.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Results extends Component{
+    render(){ 
+
+         state = {
+            name:"Mario",
+            winner:true,
+        }
+
+
+
+        return(
+            <div>
+                <h1>Bravo</h1>
+            </div>
+        )
+    }
+}
