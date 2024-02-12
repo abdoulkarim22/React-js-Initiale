64672b9f67a5fe3bfee05f07039f4de9906d77cb
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-12 11:48:01 +0100

    add height width

---
+++ react-js-initiale/src/components/Car.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import car from '../image.png'
+
+const Car = (props) =>{
+
+return <img height={props.height} width={props.width} src={car} />
+
+}
+export default Car
--- react-js-initiale/src/components/Forms.jsx
+++ react-js-initiale/src/components/Forms.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import Car from "./Car";
+
 
 class Form extends Component{
     render(){
         return(
             <div className="App">
-              Image ici
+             <Car width="700" height="700" />
               <h1>Commentaires</h1>
               Formulaires
             </div>
