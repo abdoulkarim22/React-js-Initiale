9a2de66b4c2fe44861bfa73b7e84ab3c0b770797
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-12 12:00:07 +0100

    add classname blue

--- react-js-initiale/src/components/Forms.jsx
+++ react-js-initiale/src/components/Forms.jsx
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import Car from "./Car";
 
-const titreRed = {
-    color:'red',
-    fontSize:'50px'
-}
 
 class Form extends Component{
     render(){
         return(
             <div className="App">
-              <h1 style={titreRed}>Commentaires</h1>
+              <h1  className="blue">Commentaires</h1>
               <button>Valider</button>
             </div>
         )
