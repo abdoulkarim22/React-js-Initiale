b0e6c30aeeb69ea61d3f05aac015b1ba1ee5384f
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-14 10:41:50 +0100

    i bootstrap

--- react-js-initiale/src/components/Forms.jsx
+++ react-js-initiale/src/components/Forms.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Car from "./Car";
 import './styles/Mycss.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class Form extends Component{
@@ -9,8 +10,8 @@ class Form extends Component{
         return(
             <div className="App">
                 <h1 className="blue">Commentaire</h1>
-              <p className={Myclass}>I am red or blue</p>
-              <button>Valider</button>
+              <p className={`${Myclass} bigFont`}>I am red or blue</p>
+              <button className="btn btn-danger">Valider</button>
             </div>
         )
     }
