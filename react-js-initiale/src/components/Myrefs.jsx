b5a4aa45f1ad3a6aa9fd0b4a06b26ca48621273a
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-02-14 13:08:08 +0100

    end

--- react-js-initiale/src/components/Myrefs.jsx
+++ react-js-initiale/src/components/Myrefs.jsx
@@ -4,12 +4,38 @@
 import React, { Component } from 'react'
 
  class Myrefs extends Component {
+
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         value:''
+      }
+      this.MyTitle = React.createRef();
+
+      this.Myinput = React.createRef();
+
+    }
+    update = (event) =>{
+    this.setState({
+        value: event.target.value
+    })
+    }
+    componentDidUpdate(prevProps, prevState) {
+        this.MyTitle.current.style.color = "red";
+    }
+    hanclick = () => {
+      console.log(this.Myinput.current.value);
+    }
+    
   render() {
     return (
 
       <div className='App'>
-        <h1>Valeur:</h1>
-        <input type="text" />
+        <h1 ref={this.MyTitle}>Valeur: {this.state.value}</h1>
+        {/* <input type="text" value={this.state.value} onChange={this.update} /> */}
+        <input ref={this.Myinput}    type="text" />
+        <button onClick={this.hanclick}>Valider</button>
       </div>
     )
   }
